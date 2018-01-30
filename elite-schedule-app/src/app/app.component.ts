e1bad09f0801aa02856314e1b87694818349d77b
Bruno Avelar <brunoavelar@gmail.com>
2018-01-29 23:14:36 -0500

    Side Menu Navigation

--- elite-schedule-app/src/app/app.component.ts
+++ elite-schedule-app/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MyTeamsPage } from '../pages/my-teams/my-teams';
+import { TournamentsPage } from '../pages/pages';
 
 @Component({
   templateUrl: 'app.html'
@@ -27,9 +28,11 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  goHome(){
+    this.nav.push(MyTeamsPage);
+  }
+
+  goToTournaments(){
+    this.nav.push(TournamentsPage);
   }
 }
